b9b88088260865dfb57d32434f0f1aa0061ed589
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-05-31 02:28:05 +0200

    add API endpoints for excuse management (CRUD operations)

--- DockerExcuses.WebApi/Program.cs
+++ DockerExcuses.WebApi/Program.cs
@@ -1,9 +1,15 @@
+using DockerExcuses.WebApi.Models;
+using DockerExcuses.WebApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<InMemoryDataStore>();
+builder.Services.AddScoped<IExcuseRepository, InMemoryExcuseRepository>();
 
 var app = builder.Build();
 
@@ -16,11 +22,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.UseStaticFiles();
 app.MapGet("/", context =>
 {
@@ -28,25 +29,75 @@ app.MapGet("/", context =>
     return Task.CompletedTask;
 });
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/excuses", async (IExcuseRepository excuseRepository) =>
+{
+    try
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+        var excuses = await excuseRepository.GetAllExcuses();
+        return Results.Ok(excuses);
+    }
+    catch (Exception e)
+    {
+        return Results.Problem("An error occurred while retrieving excuses: " + e.Message);
+    }
+}).WithName("GetAllExcuses");
 
+app.MapGet("/excuses/{id:int:min(0)}", async (int id, IExcuseRepository excuseRepository) =>
+{
+    try
+    {
+        var excuse = await excuseRepository.GetExcuseById(id);
+        return excuse == null
+            ? Results.NotFound()
+            : Results.Ok(excuse);
+    }
+    catch (Exception e)
+    {
+        return Results.Problem("An error occurred while retrieving the excuse: " + e.Message);
+    }
+}).WithName("GetExcuseById");
+
+app.MapPost("/excuses", async (ExcuseInputDto excuse, IExcuseRepository excuseRepository) =>
+{
+    try
+    {
+        var createdExcuse = await excuseRepository.AddExcuse(excuse);
+        return Results.Created($"/excuses/{createdExcuse.Id}", createdExcuse);
+    }
+    catch (Exception e)
+    {
+        return Results.Problem("An error occurred while adding the excuse: " + e.Message);
+    }
+}).WithName("AddExcuse");
+
+app.MapPut("/excuses/{id:int:min(0)}", async (int id, ExcuseInputDto excuse, IExcuseRepository excuseRepository) =>
+{
+    try
+    {
+        var updatedExcuse = await excuseRepository.UpdateExcuseById(id, excuse);
+        return updatedExcuse == null
+            ? Results.NotFound()
+            : Results.Ok(updatedExcuse);
+    }
+    catch (Exception e)
+    {
+        return Results.Problem("An error occurred while updating the excuse: " + e.Message);
+    }
+}).WithName("UpdateExcuse");
+
+app.MapDelete("/excuses/{id:int}", async (int id, IExcuseRepository excuseRepository) =>
+{
+    try
+    {
+        var deleted = await excuseRepository.DeleteExcuseById(id);
+        return deleted
+            ? Results.NoContent()
+            : Results.NotFound();
+    }
+    catch (Exception e)
+    {
+        return Results.Problem("An error occurred while deleting the excuse: " + e.Message);
+    }
+}).WithName("DeleteExcuse");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
